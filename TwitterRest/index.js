fb4fdd968fba07230b2fddbda173106ef2d39ed0
Marcos Sanches <58822240+markitosanches@users.noreply.github.com>
2022-09-30 13:39:09 -0400

    Seance 3

--- TwitterRest/index.js
+++ TwitterRest/index.js
@@ -2,9 +2,31 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const twit = require('twit')
+const config = require('./config.js')
 
+const Twitter = new twit(config);
 
+var params = {
+    q: '#quebec',
+    result_type: 'recent',
+    lang: 'en'
+}
 
+Twitter.get('search/tweets', params, function(err, data){
+   // console.log(data);
+   //console.log(data.statuses[0].text);
+   var newData = JSON.stringify(data);
+   fs.writeFile('tweet.json', newData, err => {
+    if(err) throw err;
+    console.log("Success");
+   })
+})
+
+app.get('/Twitter', function (req, res){
+    fs.readFile( __dirname + "/" + "tweet.json", 'utf8', function (err, data) {
+        res.end( data );
+     })
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address
